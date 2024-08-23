1e7c07b5898567e16b557a6f79f35a78c58fefd8
FlyntDenzel <flyntdenzel2004@gmail.com>
2024-08-23 14:34:19 +0100

    built channel section

--- app/Channels/page.tsx
+++ app/Channels/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ChannelHeader from '../components/ChannelHeader'
+import ChannelsList from '../components/ChannelsList'
 
 const Channels = () => {
   return (
     <div className='flex flex-col'>
       <ChannelHeader />
+      <ChannelsList />
     </div>
   )
 }
--- app/components/ChannelHeader.tsx
+++ app/components/ChannelHeader.tsx
@@ -4,11 +4,11 @@ const ChannelHeader = () => {
   return (
     <div className="flex flex-col">
       <div className="flex justify-between items-center mx-4">
-        <h1 className="text-xl font-extrabold px-5 p-4">Channels</h1>
+        <h1 className="text-2xl font-extrabold px-5 p-4">Channels</h1>
         <Plus />
       </div>
-      <h1 className="text-center text-xl">Stay updated on your favourite topics</h1>
-      <h1 className="text-center text-lg mt-1 text-wa-dark-secondary">Find channels to follow below</h1>
+      <h1 className="text-center text-lg">Stay updated on your favourite topics</h1>
+      <h1 className="text-center text-md mt-1 text-wa-dark-secondary">Find channels to follow below</h1>
     </div>
   );
 };
